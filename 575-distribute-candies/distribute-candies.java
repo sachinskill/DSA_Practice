2bcbaadbd500d4b1b9a353816e1730bb97a0a321
Sachin Gupta <145804949+sachinskill@users.noreply.github.com>
2026-07-05 02:26:31 +0530

    Time: 30 ms (93.34%) | Memory: 48.4 MB (67.74%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,14 +1,10 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
         HashSet<Integer> set=new HashSet<>();
-        int n=candyType.length;
         for(int i: candyType){
             set.add(i);
         }
-        if(set.size()<=n/2) {
-            return set.size();
-        }
 
-        return n/2;
+        return Math.min(set.size(), candyType.length/2);
     }
 }
