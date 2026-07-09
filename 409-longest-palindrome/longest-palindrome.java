d261e16632690f0cf3ed3919af0e4cec9c090103
Sachin Gupta <145804949+sachinskill@users.noreply.github.com>
2026-07-10 03:40:35 +0530

    Time: 7 ms (49.05%) | Memory: 43 MB (72.48%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -4,17 +4,17 @@ class Solution {
         for(char c: s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
-        boolean hasOdd=false;
+        
         int freq=0;
         for(int count: map.values()){
             if(count%2==0){
                 freq+=count;             
             }else{
                 freq+=count-1;
-                 hasOdd=true;
+               
             }
         }
-        if(hasOdd){
+        if(freq<s.length()){
             freq += 1;
         }
         return freq;
