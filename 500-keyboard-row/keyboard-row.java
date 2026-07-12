ffc43d021c15aa526ebd3b9706e0e1060ab90d6e
Sachin Gupta <145804949+sachinskill@users.noreply.github.com>
2026-07-12 13:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (16.67%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,39 +1,39 @@
 class Solution {
     public String[] findWords(String[] words) {
-        HashMap<Character, Integer> map=new HashMap<>();
+        int arr[]=new int[26];
         String f="qwertyuiop";
         String s="asdfghjkl";
         String z="zxcvbnm";
         for(int i=0; i<26; i++){
             if(i<10){
             char c=f.charAt(i);
-            map.put(c,1);
+            arr[c-'a']=1;
             }else if(i<19){
             char c=s.charAt(i-10);
-            map.put(c,2); 
+            arr[c-'a']=2; 
             }else{
             char c=z.charAt(i-19);
-            map.put(c,3);    
+            arr[c-'a']=3;    
             }
             
         }
-        ArrayList<String> arr=new ArrayList<>();
+        ArrayList<String> list=new ArrayList<>();
         for(int i=0; i<words.length; i++){
             String str=words[i];
             char ch=Character.toLowerCase(str.charAt(0));
-            int row=map.get(ch);
+            int row=arr[ch-'a'];
             boolean wrd=true;
             for(char c: str.toCharArray()){
                 c=Character.toLowerCase(c);
-                if(map.getOrDefault(c,0)!=row){
+                if(arr[c-'a']!=row){
                     wrd=false;
                     break;
                 }
             }
             if(wrd){
-                arr.add(words[i]);
+                list.add(words[i]);
             }
         }
-        return arr.toArray(new String[0]);
+        return list.toArray(new String[0]);
     }
 }
