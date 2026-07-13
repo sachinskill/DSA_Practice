61fc26ec33b32d09f49a3df91590ecd6c9de4f09
Sachin Gupta <145804949+sachinskill@users.noreply.github.com>
2026-07-14 02:47:38 +0530

    Time: 11 ms (90.88%) | Memory: 46.2 MB (79.89%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,33 +1,26 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int maxvowel=0,vowel=0, index=0;
+        int maxvowel=0,wind=0, index=0;
         for(int i=0; i<k; i++){
             char c=s.charAt(i);
             if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                vowel++; 
+                wind++; 
             }           
-            if(maxvowel<vowel){
-                maxvowel=vowel;
+            if(maxvowel<wind){
+                maxvowel=wind;
             }
         }
         for(int j=k; j<s.length(); j++){
-            char c=s.charAt(j);
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                int left=s.charAt(index);
-                if(left=='a'||left=='e'||left=='i'||left=='o'||left=='u'){
-                vowel=vowel+1-1;
-                }else{
-                    vowel=vowel+1;
-                }
-            }else{
-                int left=s.charAt(index);
-                if(left=='a'||left=='e'||left=='i'||left=='o'||left=='u'){
-                    vowel--;
-                }
+            char left=s.charAt(j-k);
+            if(left=='a'||left=='e'||left=='i'||left=='o'||left=='u'){
+                wind--;
             }
-            index++;
-            maxvowel=Math.max(vowel, maxvowel);
+            int right=s.charAt(j);
+            if(right=='a'||right=='e'||right=='i'||right=='o'||right=='u'){
+                wind++;
+            }
             
+            maxvowel=Math.max(maxvowel, wind);          
         }
         return maxvowel;
     }
