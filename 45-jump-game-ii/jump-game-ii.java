01c51f4b2e9cb12b3269b746e3f87ca27d203833
Sachin Gupta <145804949+sachinskill@users.noreply.github.com>
2026-09-13 21:03:33 +0530

    Time: 1 ms (99.79%) | Memory: 47.4 MB (38.72%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,29 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-            // Agar array mein 1 hi element hai, toh 0 jump chahiye
-        if (nums.length <= 1) return 0; 
-        
-        int jumps = 0;
-        int current_end = 0;   // Is jump mein hum kaha tak ja sakte hain
-        int farthest = 0;      // Agli jump ke liye sabse door ka rasta kaunsa hai
-
-        // Hum aakhri element se ek pehle tak hi loop chalayenge
-        for (int i = 0; i < nums.length - 1; i++) {
-            // Har point par check karo ki sabse door kaha pahonch sakte hain
-            farthest = Math.max(farthest, i + nums[i]);
-
-            // Jab hum pichli jump ki aakhri limit tak pahonch jayein
-            if (i == current_end) {
-                jumps++;            // Ek jump badhao
-                current_end = farthest; // Agli limit set karo
-                
-                // Agar hum aakhri element tak ya usse aage pahonch gaye, toh ruk jao
-                if (current_end >= nums.length - 1) {
-                    break;
-                }
+      int jump=0;
+      int coverage=0;
+      int lastjumpidx=0;
+      if(nums.length==1) return 0;
+      for(int i=0; i<nums.length; i++){
+        coverage=Math.max(coverage, i+nums[i]);
+        if(i==lastjumpidx){
+            lastjumpidx=coverage;
+            jump++;
+            if(coverage>=nums.length-1){
+                return jump;
             }
         }
-        return jumps;
-
+      }
+        return jump;
     }
 }
